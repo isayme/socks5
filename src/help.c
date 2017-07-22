f7bc9ea64d841f56c709e0a05dffa72a6cacf550
isayme <isaymeorg@gmail.com>
2017-07-22 15:57:14 +0800

    add help

--- src/help.c
+++ src/help.c
@@ -8,4 +8,6 @@ void help() {
     printf("   --password <password>    password for auth\n");
     printf("   -p, --port <port>        server port, default to 1080\n");
     printf("   -d                       run in daemon\n");
+    printf("   --loglevel <level>       log levels: fatal, error, warning, info, debug, trace\n");
+    printf("   -h, --help               help\n");
 }
